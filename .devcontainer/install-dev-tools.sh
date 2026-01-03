13fe4302fd5f7dcce086fe3e4b8420c64f00ab0b
Wouwmw <118289604+Wouwmw@users.noreply.github.com>
2026-01-03 22:51:02 +0100

    Initial Homey app commit

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Define homey package.json 
+npm init --quiet -y 1>/dev/null
+npm config set init-module ./.npm-init.js
+npm init --quiet -y 1>/dev/null
+
+# ESLint config for Athom B.V. JavaScript projects.
+npm install --save-dev \
+  eslint \
+  prettier \
+  eslint-config-prettier \
+  eslint-plugin-prettier \
+  eslint-config-athom
+
+npx install-peerdeps --dev \
+  eslint-config-airbnb-base
+
+# Commiting the changes 
+git add .
+# > cannot write to ./git directory, to be fixed
+# git commit -am "Dev-Tools installed"
